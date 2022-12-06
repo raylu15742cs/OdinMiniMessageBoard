eecf1d717e15f9b60b24f0219237a48aec04c229
raylu15742cs <raylucs15742@gmail.com>
2022-12-06 09:42:35 -0800

    add topic and message

--- MiniMessageBoard/routes/index.js
+++ MiniMessageBoard/routes/index.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 const messages = [
   {
-    text: 'Hi there!',
-    user: 'Amando',
+    topic: 'Welcome to the Messaging Board',
+    text: 'You can add your own message whenever you want with the new message button',
+    user: 'Raymond Lu',
     added: new Date().toDateString(),
   },
   {
-    text: 'Hello World!',
-    user: 'Charles',
+    topic: 'My First Node.JS Project',
+    text: 'This is my first Node.JS Project, I hope you like it',
+    user: 'Raymond Lu',
     added: new Date().toDateString(),
   },
 ];
@@ -21,7 +23,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/new', function(req, res, next) {
   const { text, user } = req.body;
-  messages.push({ text, user, added: new Date().toDateString() });
+  messages.push({ text,topic, user, added: new Date().toDateString() });
   res.redirect('/');
 })
 module.exports = router;
